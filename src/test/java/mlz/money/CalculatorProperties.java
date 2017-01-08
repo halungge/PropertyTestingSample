d346446cfe69129da0091dd8fb4d54805d782fdb
magdalena <magdalena>
2017-01-08 12:13:19 +0100

    set fixed seed for generators, to make results reproducible

--- src/test/java/mlz/money/CalculatorProperties.java
+++ src/test/java/mlz/money/CalculatorProperties.java
@@ -1,6 +1,7 @@
 package mlz.money;
 
 import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.When;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.runner.RunWith;
 
@@ -12,8 +13,8 @@ import static org.junit.Assert.*;
 @RunWith(JUnitQuickcheck.class)
 public class CalculatorProperties {
 
-    @Property(trials = 100)
-    public void additionIsCommutative(BigDecimal a, BigDecimal b){
+    @Property(trials = 2)
+    public void additionIsCommutative(@When(seed = 23L) BigDecimal a, @When(seed=77L)BigDecimal b){
         System.out.printf("testing with a= %2.10f  and b=%2.10f\n", a, b);
         assertEquals(Calculator.add(a, b), Calculator.add(b,a));
     }
