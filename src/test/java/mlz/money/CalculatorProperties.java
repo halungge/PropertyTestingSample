99833a6a999f4a411bce0df5ad37b6dad2ae772d
magdalena <magdalena>
2017-01-08 10:53:00 +0100

    add first property check for calculator

--- src/test/java/mlz/money/CalculatorProperties.java
+++ src/test/java/mlz/money/CalculatorProperties.java
@@ -12,9 +12,9 @@ import static org.junit.Assert.*;
 @RunWith(JUnitQuickcheck.class)
 public class CalculatorProperties {
 
-    @Property
+    @Property(trials = 100)
     public void additionIsCommutative(BigDecimal a, BigDecimal b){
-
+        System.out.printf("testing with a= %2.10f  and b=%2.10f\n", a, b);
         assertEquals(Calculator.add(a, b), Calculator.add(b,a));
     }
 
